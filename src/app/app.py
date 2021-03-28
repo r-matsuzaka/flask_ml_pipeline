64c7579d62beeee9279f40c487e74686faa4b1de
r-matsuzaka <matsuzakaryo@gmail.com>
2021-03-28 20:50:36 +0900

    add app.py

---
+++ src/app/app.py
@@ -0,0 +1,36 @@
+# Importing the libraries
+import os
+import pickle
+
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)  # create instance on flask
+model = pickle.load(open("model.pickle", "rb"))
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2)
+    return render_template(
+        "index.html", prediction_text="Profit should be ${}".format(output)
+    )
+
+
+@app.route("/predict_api", methods=["POST"])
+def predict_api():
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+    output = prediction[0]
+    return jsonify(output)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
